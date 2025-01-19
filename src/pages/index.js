cc64e5d57916ed915965fa7eccab31688669999d
Michael <michaeldiputra257@gmail.com>
2025-01-19 23:16:44 +0800

    make home responsive on tablet

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,17 +3,24 @@ import pictureBgHome from "../../public/landing/picture-bg-home.svg";
 
 export default function Home() {
   return (
-    <div className="px-20 overflow-x-hidden">
-      <section className="flex max-h-svh h-screen pt-16 w-full justify-between">
-        <div className="flex flex-col w-1/3 max-w-md mt-28">
-          <h1 className="text-secondary text-wr6xl font-extrabold text-pretty !leading-tight uppercase pb-8">Lorem ipsum dolor sit amet</h1>
-          <button className="bg-[#B08968] text-white w-fit font-extrabold py-3 px-4 rounded-2xl">Learn More</button>
-        </div>
-        <div>
-          <Image src={pictureBgHome} alt="bg-picture" className="h-full translate-x-72" />
-        </div>
-      </section>
-    </div>
+    <div className="flex justify-center px-5 md:px-12 lg:px-16 xl:px-20 overflow-x-hidden">
+      <div className="max-w-7xl w-full">
 
+        <section className="flex max-h-svh h-screen pt-16 w-full justify-between items-center">
+          <div className="flex flex-col w-full max-w-md -mt-28">
+            <h1 className="text-secondary text-wr6xl font-extrabold text-pretty !leading-tight uppercase pb-8">Lorem ipsum dolor sit amet</h1>
+            <button className="bg-[#B08968] text-white w-fit font-extrabold py-3 px-4 rounded-2xl">Learn More</button>
+          </div>
+          <div className="min-w-96 h-fit flex items-center md:-mr-60 lg:-mr-72 xl:-mr-80">
+            <Image src={pictureBgHome} alt="bg-picture" className="max-h-screen -mt-8" />
+          </div>
+        </section>
+
+        <section className="p-1 bg-black">
+
+        </section>
+
+      </div>
+    </div>
   );
 }
