f9ffdb0da02670c6fa8d3e9afba5abba03ab5e30
Michael <michaeldiputra257@gmail.com>
2025-01-21 22:56:07 +0800

    make about us component responsive on mobile and tab

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,17 +17,17 @@ export default function Home() {
         </section>
 
         <section className="flex justify-center py-20">
-          <div className="flex max-w-4xl w-full bg-[#B08968] p-7 bg-[url('/landing/rattan-bg-aboutus.png')] bg-no-repeat bg-right-bottom	">
-            <div className="max-w-80 w-full grid grid-cols-2 h-[488px] gap-4">
+          <div className="flex flex-col-reverse md:flex-row max-w-4xl w-full bg-[#B08968] p-3 sm:p-5 md:p-7 bg-[url('/landing/rattan-bg-aboutus.png')] bg-no-repeat bg-right-bottom	">
+            <div className="md:max-w-80 w-full grid grid-cols-2 h-96 sm:h-[488px] gap-1.5 sm:gap-2.5 md:gap-4">
               <div className="w-full bg-[#E6CCB2] row-span-3"></div>
               <div className="w-full bg-[#E6CCB2] row-span-2"></div>
               <div className="w-full bg-[#E6CCB2] row-span-4"></div>
               <div className="w-full bg-[#E6CCB2] row-span-1"></div>
               <div className="w-full bg-[#E6CCB2] row-span-2"></div>
             </div>
-            <div className="w-full pl-11">
-              <h5 className="font-semibold text-wrXl text-[#F9E2CB] mb-3.5">About Us</h5>
-              <h2 className="font-semibold text-wr4xl text-white font-Montserrat mb-4">Lorem ipsumdolor sit amet, consectetur adipiscing.</h2>
+            <div className="w-full pb-4 md:pb-0 md:pl-11">
+              <h5 className="font-semibold text-wrXl text-[#F9E2CB] mb-1 sm:mb-2 md:mb-3.5">About Us</h5>
+              <h2 className="font-semibold text-wr4xl text-white font-Montserrat mb-1.5 sm:mb-3 md:mb-4">Lorem ipsumdolor sit amet, consectetur adipiscing.</h2>
               <p className="font-OpenSans font-semibold text-wrBase text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sodales nisl vel eros maximus, nec bibendum nulla vestibulum. Pellentesque euismod ex odio, sed faucibus est varius quis.</p>
             </div>
           </div>
