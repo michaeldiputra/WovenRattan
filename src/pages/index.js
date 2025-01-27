8f8082213e0b2d2382e2dd5a41f62685b31c600e
Michael <michaeldiputra257@gmail.com>
2025-01-27 23:14:01 +0800

    fix hover on button

--- src/pages/index.js
+++ src/pages/index.js
@@ -113,7 +113,7 @@ export default function Home() {
             <div className="hidden max-h-svh h-screen pt-16 w-full justify-between items-center md:flex">
               <div className="flex flex-col w-full max-w-md -mt-28">
                 <h1 className="text-secondary text-wr6xl font-extrabold text-pretty leading-tight! uppercase pb-8">Lorem ipsum dolor sit amet</h1>
-                <Link href="/#product" className="bg-[#B08968] text-white w-fit font-extrabold py-3 px-4 rounded-2xl">Learn More</Link>
+                <Link href="/#product" className="bg-[#B08968] hover:bg-primary text-white w-fit font-extrabold py-3 px-4 rounded-2xl">Learn More</Link>
               </div>
               <div className="min-w-96 h-fit flex items-center -mr-60 lg:-mr-72 xl:-mr-80 relative">
                 <Image src={HomeBg} alt="bg-picture" className="max-h-screen -mt-8" />
