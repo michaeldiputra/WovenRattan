20a292e6d55dd9c3c5ed82dd81f2b985daa5332e
Michael <michaeldiputra257@gmail.com>
2025-01-27 09:25:43 +0800

    create footer component, add layout

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
 import "@/styles/globals.css";
+import Layout from"@/pages/components/Layout";
+import Footer from"@/pages/components/Footer";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+      <Footer />
+    </Layout>
+  );
 }
---
+++ src/pages/components/Footer.js
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import Image from "next/image";
+import facebook from "../../../public/components/facebook.svg";
+import instagram from "../../../public/components/instagram.svg";
+import twitter from "../../../public/components/twitter.svg";
+import whatsapp from "../../../public/components/whatsapp.svg";
+
+export default function Footer(){
+    return(
+        <footer className="py-29 flex justify-center">
+            <div className="text-center flex flex-col items-center">
+                <h1 className="font-medium font-Montserrat text-2xl mb-17">
+                    Logo
+                </h1>
+                <div className="font-Montserrat flex gap-13 px-12 mb-10 text-gray-700">
+                    <Link href="/#home" className="font-extrabold text-wrXl">Home</Link>
+                    <Link href="/#home" className="font-extrabold text-wrXl">About</Link>
+                    <Link href="/#home" className="font-extrabold text-wrXl">Product</Link>
+                    <Link href="/#home" className="font-extrabold text-wrXl">Contact Us</Link>
+                </div>
+                <div className="h-px w-full bg-black/37 mb-6"></div>
+                <div className="flex gap-12 max-h-8">
+                    <Image src={twitter} width={100} height={100} />
+                    <Image src={instagram} width={100} height={100} />
+                    <Image src={whatsapp} width={100} height={100} />
+                    <Image src={facebook} width={100} height={100} />
+                </div>
+            </div>
+        </footer>
+    )
+}
