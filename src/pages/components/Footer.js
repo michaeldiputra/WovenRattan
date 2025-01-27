f587d64a910b971cd4e8a7f7e8dc844d629fea77
Michael <michaeldiputra257@gmail.com>
2025-01-27 13:21:41 +0800

    make footer responsive

--- src/pages/components/Footer.js
+++ src/pages/components/Footer.js
@@ -7,23 +7,23 @@ import whatsapp from "../../../public/components/whatsapp.svg";
 
 export default function Footer(){
     return(
-        <footer className="py-29 flex justify-center">
-            <div className="text-center flex flex-col items-center">
+        <footer className="pb-6 sm:pb-29 pt-29 flex sm:justify-center landing-container">
+            <div className="sm:text-center flex flex-col sm:items-center max-w-7xl w-full sm:w-fit">
                 <h1 className="font-medium font-Montserrat text-2xl mb-17">
                     Logo
                 </h1>
-                <div className="font-Montserrat flex gap-13 px-12 mb-10 text-gray-700">
+                <div className="font-Montserrat flex flex-col sm:flex-row gap-y-2 gap-x-13 sm:px-12 mb-5 sm:mb-10 text-gray-700">
                     <Link href="/#home" className="font-extrabold text-wrXl">Home</Link>
                     <Link href="/#home" className="font-extrabold text-wrXl">About</Link>
                     <Link href="/#home" className="font-extrabold text-wrXl">Product</Link>
                     <Link href="/#home" className="font-extrabold text-wrXl">Contact Us</Link>
                 </div>
                 <div className="h-px w-full bg-black/37 mb-6"></div>
-                <div className="flex gap-12 max-h-8 max-w-3xs">
-                    <Image src={twitter} width={100} height={100} />
-                    <Image src={instagram} width={100} height={100} />
-                    <Image src={whatsapp} width={100} height={100} />
-                    <Image src={facebook} width={100} height={100} />
+                <div className="flex gap-5 sm:gap-12 max-h-5 sm:max-h-8 max-w-3xs justify-start">
+                    <Image src={twitter} width={100} height={100} className="h-full w-fit" />
+                    <Image src={instagram} width={100} height={100} className="h-full w-fit" />
+                    <Image src={whatsapp} width={100} height={100} className="h-full w-fit" />
+                    <Image src={facebook} width={100} height={100} className="h-full w-fit" />
                 </div>
             </div>
         </footer>
